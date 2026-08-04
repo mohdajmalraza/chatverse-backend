e81eced07eb79cfea4acda0a95b4d263789825f6
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2026-08-04 16:04:37 +0530

    feat(chat): Implement real-time chat feature using Socket.IO and MongoDB

--- routes/auth.js
+++ routes/auth.js
@@ -10,14 +10,12 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-
   try {
     const existingUser = await User.findOne({ username });
-
     if (existingUser) {
       return res
         .status(400)
-        .json({ message: "User already exists. Please Login." });
+        .json({ message: "User already exists. Please login." });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -27,40 +25,31 @@ router.post("/register", async (req, res) => {
     await user.save();
 
     const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "4h" });
-
-    return res.status(201).json({
-      token,
-      username,
-    });
+    res
+      .status(201)
+      .json({ message: "User registered successfully.", token, username });
   } catch (error) {
-    return res.status(500).json({ message: "Server Error", error: error });
+    res.status(500).json({ message: "Server Error", error: error });
   }
 });
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-
   try {
     const user = await User.findOne({ username });
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found." });
-    }
+    if (!user) return res.status(404).json({ message: "User not found." });
 
     const isPasswordMatch = await user.comparePassword(password);
-
-    if (!isPasswordMatch) {
+    if (!isPasswordMatch)
       return res.status(400).json({ message: "Invalid credentials" });
-    }
 
-    return res.status(200).json({
-      message: "Login successfull.",
-      username: user.username,
-    });
+    res
+      .status(200)
+      .json({ message: "Login successfull", username: user.username });
   } catch (error) {
-    return res
+    res
       .status(500)
-      .json({ message: "Server error while login", error: error });
+      .json({ message: "Server error while login.", error: error });
   }
 });
 

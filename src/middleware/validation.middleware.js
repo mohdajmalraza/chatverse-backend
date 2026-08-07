5d63cbde6b5d9317ac9c287820da3b6e422d361c
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2026-08-07 21:48:34 +0530

    refactor(validation): pass request object to validators

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -1,9 +1,25 @@
 import ApiError from "../utils/ApiError.js";
 
+// const validate = (validator) => {
+//   return (req, res, next) => {
+//     try {
+//       validator(req.body);
+//       next();
+//     } catch (error) {
+//       if (error instanceof ApiError) {
+//         return next(error);
+//       }
+
+//       return next(new ApiError(400, error.message));
+//     }
+//   };
+// };
+
 const validate = (validator) => {
   return (req, res, next) => {
     try {
-      validator(req.body);
+      validator(req);
+
       next();
     } catch (error) {
       if (error instanceof ApiError) {

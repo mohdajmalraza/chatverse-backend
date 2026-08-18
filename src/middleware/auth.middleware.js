7b7a1e5dd14028ccd64d520fcebd23b668f1eea6
Mohd Ajmal Raza <mohdajmalraza92786@gmail.com>
2026-08-18 18:57:19 +0530

    fix(deployment): add async handler utility

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,56 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import ApiError from "../utils/ApiError.js";
-import asyncHandler from "../utils/asyncHandler.js";
-
-// const protect = async (req, res, next) => {
-//   try {
-//     // Get token from Authorization header
-//     const authHeader = req.headers.authorization;
-
-//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//       throw new ApiError(401, "Access denied. No token provided.");
-//     }
-
-//     // Extract token
-//     const token = authHeader.split(" ")[1];
-
-//     // Verify token
-//     let decoded;
-
-//     try {
-//       decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     } catch (error) {
-//       if (error.name === "TokenExpiredError") {
-//         throw new ApiError(401, "Token has expired.");
-//       }
-
-//       throw new ApiError(401, "Invalid token.");
-//     }
-
-//     // Find user
-//     const user = await User.findById(decoded.id).select("-password");
-
-//     if (!user) {
-//       throw new ApiError(404, "User not found.");
-//     }
-
-//     // Attach authenticated user to request
-//     req.user = {
-//       id: user._id,
-//       name: user.name,
-//       email: user.email,
-//     };
-
-//     next();
-//   } catch (error) {
-//     if (error instanceof ApiError) {
-//       return next(error);
-//     }
-
-//     return next(new ApiError(500, "Internal server error."));
-//   }
-// };
 
 const protect = async (req, res, next) => {
   try {
@@ -89,46 +39,3 @@ const protect = async (req, res, next) => {
     next(error);
   }
 };
-
-// const protect = asyncHandler(async (req, res, next) => {
-//   // Get token from Authorization header
-//   const authHeader = req.headers.authorization;
-
-//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//     throw new ApiError(401, "Access denied. No token provided.");
-//   }
-
-//   // Extract token
-//   const token = authHeader.split(" ")[1];
-
-//   // Verify token
-//   let decoded;
-
-//   try {
-//     decoded = jwt.verify(token, process.env.JWT_SECRET);
-//   } catch (error) {
-//     if (error.name === "TokenExpiredError") {
-//       throw new ApiError(401, "Token has expired.");
-//     }
-
-//     throw new ApiError(401, "Invalid token.");
-//   }
-
-//   // Find user
-//   const user = await User.findById(decoded.id).select("-password");
-
-//   if (!user) {
-//     throw new ApiError(404, "User not found.");
-//   }
-
-//   // Attach authenticated user to request
-//   req.user = {
-//     id: user._id,
-//     name: user.name,
-//     email: user.email,
-//   };
-
-//   next();
-// });
-
-export default protect;
